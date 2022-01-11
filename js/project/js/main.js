2cb50491ac1603b6221d5247d0062494ba961384
Suhyeon Jeon <shjeonlab@gmail.com>
2022-01-11 22:04:43 +0900

    comment: fix main.js

--- js/project/js/main.js
+++ js/project/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+	
+	// bxSlider : jQuery 기반의 반응형 이미지 슬라이더 플러그인
 	$(".main-visual-slide").bxSlider({
 		auto:true,
 		pause:2000,
@@ -8,6 +10,7 @@ $(function(){
 	});
 
 	$("#notice-tab-wrap h4 a").on("click", tabmenu);
+	
 	function tabmenu(e) {
 		e.preventDefault();
 		var $ts = $(this);
@@ -19,9 +22,10 @@ $(function(){
 			$next.show();
 		}
 	}
-
-	$('.grid').isotope({
-		// options
+	
+	// isotope : 항목별로 볼 수 있는 필터와 이미지 정렬을 해주는 플러그인
+	// 포트폴리오, 제품 소개 항목에 주로 사용한다.
+	$('.grid').isotope({		
 		itemSelector: '.grid-item',
 		layoutMode: 'fitRows'
 	});    
